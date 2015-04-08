d9b4c388696cb0c9c27a9d686b8bf21138bc121c
Mark Story <mark@mark-story.com>
2015-04-07 22:56:27 -0400

    Add file headers.

--- tests/TestCase/Output/AssetCacherTest.php
+++ tests/TestCase/Output/AssetCacherTest.php
@@ -1,4 +1,16 @@
 <?php
+/**
+ * MiniAsset
+ * Copyright (c) Mark Story (http://mark-story.com)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Mark Story (http://mark-story.com)
+ * @since         0.0.1
+ * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ */
 namespace MiniAsset\Test\TestCase\Output;
 
 use MiniAsset\Output\AssetCacher;
