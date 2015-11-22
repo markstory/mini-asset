ca8e11f8a1b4835beabd61be0d1fec97934ad9ec
Mark Story <mark@mark-story.com>
2015-11-22 09:50:51 -0500

    Update doc string.

--- src/Filter/LessDotPHP.php
+++ src/Filter/LessDotPHP.php
@@ -8,7 +8,7 @@
  * Redistributions of files must retain the above copyright notice.
  *
  * @copyright     Copyright (c) Mark Story (http://mark-story.com)
- * @since         0.0.1
+ * @since         1.0.2
  * @license       http://www.opensource.org/licenses/mit-license.php MIT License
  */
 namespace MiniAsset\Filter;
