94520fa0ff7d7af7295aa10ce4f389e7f6260909
Mark Story <mark@mark-story.com>
2020-11-24 11:42:21 -0500

    Fix phpcs warning

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -11,10 +11,10 @@
  * @since     0.0.1
  * @license   http://www.opensource.org/licenses/mit-license.php MIT License
  */
-/**
- * Test suite bootstrap.
- */
+
+// phpcs:disable
 require_once 'vendor/autoload.php';
+// phpcs:enable
 
 // Path constants to a few helpful things.
 define('DS', DIRECTORY_SEPARATOR);
