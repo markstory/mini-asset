f44bb6da0cffc417d60d3d16f6e64d7f59c94e37
Mark Story <mark@mark-story.com>
2024-01-27 10:46:25 -0500

    Fix phpcs

--- src/Filter/CleanCss.php
+++ src/Filter/CleanCss.php
@@ -1,4 +1,6 @@
 <?php
+declare(strict_types=1);
+
 /**
  * MiniAsset
  * Copyright (c) Mark Story (http://mark-story.com)
@@ -20,37 +22,37 @@ namespace MiniAsset\Filter;
  */
 class CleanCss extends AssetFilter
 {
-
     /**
      * Settings for CleanCss
      *
      * - `node` Path to nodejs on your machine
      * - `node_path` The path to the node_modules directory where cleancss is installed.
      */
-    protected $_settings = array(
+    protected $_settings = [
         'node' => '/usr/local/bin/node',
         'cleancss' => '/usr/local/bin/clean-css-cli/bin/cleancss',
         'node_path' => '/usr/local/lib/node_modules',
         'options' => '',
-    );
+    ];
 
     /**
      * Run `cleancss` against the output and compress it.
      *
-     * @param  string $target   Name of the file being generated.
-     * @param  string $content The uncompressed contents for $filename.
+     * @param string $target   Name of the file being generated.
+     * @param string $content The uncompressed contents for $filename.
      * @return string Compressed contents.
      */
-    public function output($target, $content)
+    public function output(string $target, string $content): string
     {
         $tmpFile = tempnam(sys_get_temp_dir(), 'CLEANCSS');
         file_put_contents($tmpFile, $content);
         $cmd = $this->_settings['node'] . ' ' .
             $this->_settings['cleancss'] . $this->_settings['options'] . ' ' .
             escapeshellarg($tmpFile);
-        $env = array('NODE_PATH' => $this->_settings['node_path']);
+        $env = ['NODE_PATH' => $this->_settings['node_path']];
         $result = $this->_runCmd($cmd, '', $env);
         unlink($tmpFile);
+
         return $result;
     }
 }
